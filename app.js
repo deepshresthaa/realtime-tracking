f00732e8bbcffe333956d9841567e63a08fcfb05
Deep <dipstha1313@gmail.com>
2025-07-16 11:38:01 +0545

    done

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+const server = http.createServer(app);
+const io = socketio(server);
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id);
+  });
+});
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => console.log("listening to port 3000"));
